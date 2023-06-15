f6d66c9f065f90e3fe823a0aa0205af9bce8c7a8
nnisarggada <gadannisarg@gmail.com>
2023-06-15 17:16:43 +0530

    fixed poster

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from "react";
 const Awards = () => {
   return (
     <div id="about" className="h-section flex flex-col items-center]">
-      <div className="bg-secondary flex flex-col items-center w-full p-6 py-12 gap-10 h-full md:justify-evenly lg:flex-row">
+      <div className="bg-secondary flex flex-col items-center w-full p-6 py-12 gap-10 h-1/2 md:h-1/2 md:justify-evenly lg:flex-row">
         <h1 className="text-primary text-3xl font-bold text-center lg:text-5xl">
           About The Play
         </h1>
@@ -15,33 +15,19 @@ const Awards = () => {
           able to help each other?
         </h2>
       </div>
-      <div className="bg-secondary flex flex-col items-center w-full p-6 py-12 gap-10 h-full md:justify-evenly lg:flex-row">
-        <h1 className="text-primary text-3xl font-bold text-center lg:text-5xl">
-          Credits
-        </h1>
-        <h2 className="text-primary text-sm text-center max-w-2xl md:text-xl lg:text-justify lg:text-2xl">
-          Gujarati Translation (Mentor): Prof. Dipti Buch
-          <br />
-          Lighting design & operation : Amogh Pisal
-          <br />
-          Music operation: Shubham Bhanushali Song Mix &
-          <br />
-          Master: Aditya Kumar, Rudransh Thakur
-          <br />
-          Song Composition & Vocals: Harnish Dave
-          <br />
-          Song Production: Manan Mehta, Harsh Joisar
-          <br />
-          Background vocals: Glory Massey
-          <br />
-          Makeup: Jitendra Shinde
-          <br />
-          Voice over: Siddharth Dhakan & Dharmendra Gohil
-          <br />
-          Production Manager: Vedika Sethi, Harshil Savla
-          <br />
-          An Artistic Humans Production
-        </h2>
+      <div className="w-screen flex flex-col justify-evenly h-1/2 bg-[#000] md:h-1/2 md:flex-row">
+        <img
+          src={require("../assets/winner1.jpg")}
+          alt="awards"
+          loading="lazy"
+          className="h-1/2 object-contain md:h-full md:w-1/2"
+        />
+        <img
+          src={require("../assets/winner2.jpg")}
+          alt="awards"
+          loading="lazy"
+          className="h-1/2 object-contain md:h-full md:w-1/2"
+        />
       </div>
     </div>
   );
